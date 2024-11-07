eeb61fd17274460de97aa60927997cd1fdc26dd9
bachu8691 <vishruth.ravikanti@gmail.com>
2024-11-07 18:49:42 +0530

    Add json2html function

---
+++ json2html.js
@@ -0,0 +1,24 @@
+export default function json2html(data) {
+    const headers = [...new Set(data.flatMap(Object.keys))];
+    
+    let html = '<table data-user="vishruth.ravikanti@gmail.com">';
+    
+    html += '<thead><tr>';
+    headers.forEach(header => {
+      html += `<th>${header}</th>`;
+    });
+    html += '</tr></thead>';
+    
+    html += '<tbody>';
+    data.forEach(row => {
+      html += '<tr>';
+      headers.forEach(header => {
+        html += `<td>${row[header] || ''}</td>`;
+      });
+      html += '</tr>';
+    });
+    html += '</tbody></table>';
+    
+    return html;
+  }
+  
